4c4d4634206da04d0c4e158fd714fe1cab97a4e2
Afia Abedin <63110122+afia-abedin@users.noreply.github.com>
2023-03-15 02:00:53 -0600

    Delete 503_ex3 directory

--- 503_ex3/run_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Running exercise 3"
-
-dts duckiebot demo --demo_name lane_following --duckiebot_name $BOT --package_name duckietown_demos --image duckietown/dt-core:daffy-arm64v8
-dts duckiebot demo --demo_name deadreckoning --duckiebot_name $BOT --package_name duckietown_demos --image duckietown/dt-core:daffy-arm64v8
